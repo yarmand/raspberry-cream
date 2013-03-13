c87ded61b6d460e4d3a73f93b278bb10085cab2c
David Waller <dwaller@yammer-inc.com>
2013-03-13 13:32:23 -0700

    Use jQuery 1.9.1 & bootstrap 2.3.1

--- ruby/app.rb
+++ ruby/app.rb
@@ -3,6 +3,11 @@ require 'bundler'
 
 Bundler.require
 
+configure do
+  set :public_folder, Proc.new { File.join(root, "static") }
+  enable :sessions
+end
+
 get '/' do
-  'Hello world'
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
