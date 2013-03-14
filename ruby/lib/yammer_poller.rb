0c6d13874acc008b1ff7710b87684238e960af83
David Waller <dwaller@yammer-inc.com>
2013-03-13 20:58:59 -0700

    Yammer API polling

---
+++ ruby/lib/yammer_poller.rb
@@ -0,0 +1,45 @@
+require 'pp'
+class YammerPoller
+  POLL_INTERVAL = 20 # seconds
+
+  def initialize
+    if File.exists?('token')
+      @auth_token = File.read('token')
+
+      # Protect aginst empty file / nonsense contents
+      @auth_token = nil if @auth_token.length < 10
+      puts "Poller using auth token '#{@auth_token}'"
+    end
+  end
+
+  def authorised?
+    !!@auth_token
+  end
+
+  def poll
+    raise "Not Authorised" unless authorised?
+    loop do 
+      limit = @last_msg_id ? "newer_than=#@last_msg_id" : "limit=10"
+      response = HTTParty.get("https://www.yammer.com/api/v1/messages/following.json?access_token=#{@auth_token}&#{limit}")
+
+      if response.code == 200
+        json = response.parsed_response
+        messages = json['messages']
+        messages.each { |msg| puts "#{msg['id']} => #{msg['body']['plain']}" }
+        pp json
+
+        # Remember where we got to, so that we can fetch only new messages.
+        if messages.first
+          @last_msg_id = messages.first['id']
+          puts "Last msg: #{@last_msg_id}"
+        else
+          puts "No new messages"
+        end
+      else
+        raise "Unexpected response code #{response.code}"
+      end
+
+      sleep(POLL_INTERVAL)
+    end
+  end
+end
